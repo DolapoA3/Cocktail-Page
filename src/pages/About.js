9b9056b32ae8e4622388dcc41df518794a8511ac
LeoKing33 <lapo8387@gmail.com>
2022-10-02 19:56:51 -0500

    Cocktail Website: home, error, about pages

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+        voluptatem tempora dignissimos debitis provident iste nulla magni
+        corporis quaerat facilis eius aspernatur minus distinctio architecto,
+        autem vel id temporibus maiores.
+      </p>
+    </section>
+  );
+};
+
+export default About;
